6102dd49234bd2da497de689a1c04b5c217e736a
rickyhidayatt <rickyhyouth@gmail.com>
2023-02-27 15:23:21 +0700

    [UPDATE] fitur send money beres

--- main.go
+++ main.go
@@ -2,42 +2,52 @@ package main
 
 import (
 	"e-wallet/config"
-	"e-wallet/model"
 	"e-wallet/repository"
-	"e-wallet/utils"
+	"e-wallet/usecase"
 	"fmt"
-	"time"
 )
 
 func main() {
 	config := config.NewConfig()
 	db := config.DbConnect()
 
-	// userRepo := repository.NewUserRepository(db)
+	userRepo := repository.NewUserRepository(db)
 
 	// fmt.Println(userRepo.GetUserById("53-331-6070"))
 	txRepo := repository.NewTransactionRepository(db)
-	// txUsecase := usecase.NewTransactionUseCase(txRepo, userRepo)
+	txUsecase := usecase.NewTransactionUseCase(txRepo, userRepo)
 
 	userId := "53-331-6070"
 	addBalance := 10000
 
-	saveTransaction := model.Transaction{
-		Id:              utils.GenerateId(),
-		UserId:          userId,
-		TransactionDate: time.Now(),
-		TransactionType: "Test transaksi tipe",
-		Amount:          addBalance,
-		ReciverId:       "b86b9f9bed9e4504a65295f93e9eb23c",
-		CategoryId:      "CategoryId1",
+	err := txUsecase.SendMoney(userId, addBalance, "test aja", "menabung", "80280239298", "diki")
+
+	if err != nil {
+		fmt.Println("Gagal")
+	} else {
+		fmt.Println("berhasil")
 	}
 
-	err := txRepo.SaveTransaction(&saveTransaction)
-	if err != nil {
-		fmt.Println("gagal transaction")
-	} else {
-		fmt.Println("Berhasil")
-	}
+	//save transaksi
+	//=====================
+	// saveTransaction := model.Transaction{
+	// 	Id:              utils.GenerateId(),
+	// 	UserId:          userId,
+	// 	TransactionDate: time.Now(),
+	// 	TransactionType: "Test transaksi tipe",
+	// 	Amount:          addBalance,
+	// 	ReciverId:       "b86b9f9bed9e4504a65295f93e9eb23c",
+	// 	CategoryId:      "CategoryId1",
+	// }
+
+	// err := txRepo.SaveTransaction(&saveTransaction)
+	// if err != nil {
+	// 	fmt.Println("gagal transaction")
+	// } else {
+	// 	fmt.Println("Berhasil")
+	// }
+	//=====================
+
 	//=====================================
 	// saveReciver := model.Receiver{
 	// 	Id:            utils.GenerateId(),
--- usecase/transaction_usecase.go
+++ usecase/transaction_usecase.go
@@ -1,13 +1,19 @@
 package usecase
 
 import (
+	"e-wallet/model"
 	"e-wallet/repository"
+	"e-wallet/utils"
+	"errors"
 	"fmt"
+	"log"
+	"time"
 )
 
 // BUG Masih Ada
 type TransactionUseCase interface {
 	AddWallet(userId string, addBalance int) (int, error)
+	SendMoney(userId string, amount int, bankName string, category string, accountNumber string, receiverName string) error
 }
 
 type transactionUseCase struct {
@@ -27,11 +33,8 @@ func (tx *transactionUseCase) AddWallet(userId string, addBalance int) (int, err
 
 	if addBalance < 10000 {
 		fmt.Println("gagal balance")
-		return 0, fmt.Errorf("addBalance amount (%d) exceeds maximum limit of 10000", addBalance)
+		return 0, errors.New("specify a Rp.10,000 minimum balance")
 	}
-
-	// var balance model.Balances
-	// fmt.Println("Jumlahnya :", balance.Balance)
 
 	err = tx.transactionRepo.AddBalance(userId, addBalance)
 	if err != nil {
@@ -39,6 +42,70 @@ func (tx *transactionUseCase) AddWallet(userId string, addBalance int) (int, err
 	}
 
 	return addBalance, nil
+}
+
+func (tx *transactionUseCase) SendMoney(userId string, amount int, bankName string, category string, accountNumber string, receiverName string) error {
+	user, err := tx.userRepo.GetUserById(userId)
+	if user == nil {
+		log.Fatal("failed to get user by id", err)
+		return err
+	}
+
+	if amount < 5000 {
+		log.Fatal("The minimum amount is 5.000")
+		return err
+	}
+
+	balances, err := tx.transactionRepo.GetBalance(userId)
+	if err != nil {
+		log.Fatal("failed to get user balances", err)
+		return err
+	}
+
+	for _, balance := range balances {
+		if balance < amount {
+			log.Fatal(" There are insufficient funds on your account; ")
+			return err
+		}
+	}
+
+	receiver := model.Receiver{
+		Id:            utils.GenerateId(),
+		UserId:        userId,
+		Name:          receiverName,
+		BankName:      bankName,
+		AccountNumber: accountNumber,
+	}
+
+	err = tx.transactionRepo.SaveReceiver(&receiver)
+	if err != nil {
+		return err
+	}
+
+	transaction := model.Transaction{
+		Id:              utils.GenerateId(),
+		UserId:          userId,
+		TransactionDate: time.Now(),
+		TransactionType: bankName,
+		Amount:          amount,
+		ReciverId:       receiver.Id,
+		CategoryId:      category,
+	}
+	err = tx.transactionRepo.SaveTransaction(&transaction)
+
+	if err != nil {
+		log.Fatal("failed to save transaction", err)
+		return err
+	}
+
+	err = tx.transactionRepo.SendBalance(userId, amount)
+
+	if err != nil {
+		log.Fatal("failed to send balance", err)
+		return err
+	}
+
+	return nil
 }
 
 func NewTransactionUseCase(txRepoArg repository.TransactionRepository, userArg repository.UserRepository) TransactionUseCase {
