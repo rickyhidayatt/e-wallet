5152049eee50bdedea60762a1e5269428d179cc6
rickyhidayatt <rickyhyouth@gmail.com>
2023-03-01 20:51:04 +0700

    [BUG] Bug di server saat penambahan new controller

--- delivery/controller/user_controller.go
+++ delivery/controller/user_controller.go
@@ -1,7 +1,10 @@
 package controller
 
 import (
+	"e-wallet/model"
 	"e-wallet/usecase"
+	"e-wallet/utils"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +13,26 @@ type UserController struct {
 	userUseCase usecase.UserUseCase
 }
 
-// func (uc *UserController)GetAllUser
+func (uc *UserController) Login(c *gin.Context) {
+	var login model.User
+	err := c.ShouldBindJSON(&login)
+
+	if err != nil {
+		response := utils.ApiResponse("server error", http.StatusBadRequest, "error", err.Error())
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	login, err = uc.userUseCase.Login(login.Email, login.Password)
+	if err != nil {
+		response := utils.ApiResponse("login failed for user", http.StatusInternalServerError, "error", err.Error())
+		c.JSON(http.StatusInternalServerError, response)
+	} else {
+		response := utils.ApiResponse("successful login to your account", http.StatusOK, "success", login)
+		c.JSON(http.StatusOK, response)
+	}
+
+}
 
 func NewUserController(router *gin.Engine, userArg usecase.UserUseCase) *UserController {
 	userController := UserController{
@@ -18,6 +40,6 @@ func NewUserController(router *gin.Engine, userArg usecase.UserUseCase) *UserCon
 	}
 
 	r := router.Group("api/user")
-	r.GET("")
+	r.POST("/login", userController.Login)
 	return &userController
 }
--- model/user.go
+++ model/user.go
@@ -5,12 +5,17 @@ import "time"
 type User struct {
 	Id             string
 	Name           string
-	Email          string
+	Email          string `json:"email" binding:"required,email"`
 	PhoneNumber    string `db:"phone_number"`
-	Password       string
+	Password       string `json:"password" binding:"required"`
 	Address        string
 	BirthDate      time.Time `db:"birth_date"`
 	ProfilePicture string    `db:"profile_picture"`
 	CreatedAt      time.Time `db:"created_at"`
 	UpdateAt       time.Time `db:"update_at"`
+}
+
+type UserLogin struct {
+	Email    string
+	Password string
 }
